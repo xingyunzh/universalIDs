35f0baa0ecf4ce5d504c41034608deb30b575afb
rayzhang-xingyunzh <ray.zhang@xingyunzh.com>
2017-01-09 17:49:07 +0800

    bug fix

--- app/authenticate/wechat-auth.js
+++ app/authenticate/wechat-auth.js
@@ -1,9 +1,9 @@
 var OAuth = require('wechat-oauth');
 var systemConfigRepository = require('../repositories/systemConfigRepository');
-var client = new OAuth('wx5ce7696222e79ca5', 'f9b1976f789b15a56adc6775353cecab');
+//var client = new OAuth('wx5ce7696222e79ca5', 'f9b1976f789b15a56adc6775353cecab');
 
 exports.getClientByApp = function(app){
-	return systemConfigRepository.getWechatCredentials(function(wechat){
+	return systemConfigRepository.getWechatCredentials().then(function(wechat){
 		var credentials = wechat[app];
 		var client = new OAuth(credentials.appid,credentials.secret);
 		return client;
